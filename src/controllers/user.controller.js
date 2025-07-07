3f12e09a1f61414e97d4f39cf64be6ae655966d6
Mayank Pandey <mayank.pandey.external@airbus.com>
2025-07-07 17:32:34 +0530

    Channel Profile and watch history pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import {ApiError} from "../utils/ApiError.js"
 import {User} from "../models/user.models.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshToken = async (userId) => {
     try {
@@ -186,7 +187,7 @@ const updateAccountDetails = asyncHandler (async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const user = User.findByIdAndUpdate(req.user?._id,
+    const user = await User.findByIdAndUpdate(req.user?._id,
                            {
                             $set: {
                                 fullname: fullname,
@@ -240,4 +241,110 @@ const updateCoverImage = asyncHandler( async (req, res) => {
               .json(new ApiResponse(200, {user}, "Cover image updated successfully"))
 })
 
-export {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateAvatarImage, updateCoverImage}
+const getUserChannelProfile = asyncHandler( async (req, res) => {
+    const {username} = req.params
+    if(!username?.trim()) throw new ApiError("Username not found")
+    
+    const channel = User.aggregate([
+                            {
+                                $match: {username: username?.toLowerCase()}
+                            },
+                            {
+                                $lookup: {      //who follows this user
+                                    from: "subscriptions",
+                                    localField: "_id",
+                                    foreignField: "channel",   //channel is also a user, which stores only the object id of the user to which it refers, and not the entire user object 
+                                    as: "subscribers"
+                                }
+                            }, 
+                            {
+                                $lookup: {    //who this user follows
+                                    from: "subscriptions",
+                                    localField: "_id",
+                                    foreignField: "subscriber",   //subscriber is also a user, which stores only the object id of the user to which it refers, and not the entire user object
+                                    as: "subscribedTo"
+                                }
+                            }, 
+                            {
+                                $addFields: {
+                                    subscribersCount: {$size: "$subscribers"},
+                                    channelsSubscribedToCount: {$size: "$subscribedTo"},
+                                    isSubscribed: {$cond: {   //whether the current logged-in user is subscribed to this channel
+                                        if: {$in: [req.user?.id, "$subscribers.subscriber"]},
+                                        then: true,
+                                        else: false
+                                    }}
+                                }
+                            },
+                            {
+                                $project: {
+                                    fullname: 1,
+                                    username: 1,
+                                    isSubscribed: 1,
+                                    subscribersCount: 1,
+                                    channelsSubscribedToCount: 1,
+                                    isSubscribed: 1,
+                                    coverImage: 1,
+                                    avatar: 1,
+                                    email: 1 
+                                }
+                            }
+                        ])
+    if(!channel?.length) throw new ApiError(404 ,"Channel does not exist")   
+    
+    return res.status(200)
+            .json(new ApiResponse(200, channel[0], "User channel fetched successfully"))
+})
+
+const getUserWatchHistory = asyncHandler( async(req, res) => {
+    const user = await User.aggregate([
+        { $match: { _id: new mongoose.Types.ObjectId(req.user._id) } },
+        { $lookup: {
+            from: "videos",
+            localField: "watchHistory",
+            foreignField: "_id",
+            as: "watchHistory",
+            pipeline: [
+                {
+                    $lookup: {
+                        from: "users",
+                        localField: "owner",
+                        foreignField: "_id",
+                        as: "owner",
+                        pipeline: [
+                            {
+                                $project: {
+                                    fullname: 1,
+                                    username: 1,
+                                    avatar: 1
+                                }
+                            }
+                        ]
+                    }
+                },
+                {
+                    $addFields: {
+                        owner: {
+                            $first: "$owner"
+                        }
+                    }
+                }
+            ]
+        }},
+    ])
+
+    return res.status(200)
+              .json(new ApiResponse(200, user[0].watchHistory, "Watch history fetched successfully"))
+})
+
+export {registerUser, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateAvatarImage, 
+    updateCoverImage,
+    getUserChannelProfile,
+    getUserWatchHistory}
